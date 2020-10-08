db6810912c4402734730c3e989c80f105257f920
srv.dwivedi <sourav.dwivedi@auw.co.in>
2020-10-08 21:41:56 +0530

    printing data form list and nested list

--- src/index.js
+++ src/index.js
@@ -3,24 +3,50 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import * as serviceWorker from './serviceWorker';
 
-let details = {
-  name : "sourav",
-  lname : "dwivedi",
-  company : "AUW",
+
+
+// Code to use map in React :
+
+  // const waterBody = ['lake','ocean','river'];
+
+  // function App( { lakes }){
+  //   return (
+  //     <ul> {lakes.map(lake => (
+  //       <li> { lake }</li>
+  //   ))} </ul>
+  //   )
+  // }
+
+  // ReactDOM.render(
+  //   <App lakes = {waterBody} />,
+  //   document.getElementById('root')
+  // );
+
+// close 
+
+
+const details = [
+  { name : "sourav" , lname : "dwivedi" , loc : "GGN"},
+  { name : "Ajay" , lname : "Gulati" , loc : "Nai Basti"},
+  { name : "Manan" , lname : "Arora" , loc : "Ram Nagar"},
+];
+
+function App2( { details }) {
+  return (
+    <div> 
+      {details.map(detail => (
+        <div>  
+          <h2> { detail.name }</h2>
+          <p> {detail.lname} </p>
+        </div>
+      ) )}
+
+    </div>
+  )
 }
 
-function Hello(props) {
- return ( 
-   <div> 
-    <h1> Welcome to {props.team}</h1>
- <p> Lets build something {props.message}</p>
-   </div>
- )
-}
-
-ReactDOM.render(
-
-  <Hello team="Gurgoan" message="Great"/>,
+ReactDOM.render (
+  <App2 details = {details}> </App2>,
   document.getElementById('root')
 );
 
